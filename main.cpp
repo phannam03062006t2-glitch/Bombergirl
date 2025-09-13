400ea01618a66c6f2d26e7b0f28c12ba2d9126a2
Duy Nam <phannam03062006t2@gmail.com>
2025-09-13 22:10:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 #include <iostream>
 using namespace std;
 
+class Player{
+	private:
+    protect:
+    public:
+};
+
+class Enermy : public Player{
+
+};
+
+class Bomb{
+
+};
+
 int main(){
 	
 }
+
 
 
 
