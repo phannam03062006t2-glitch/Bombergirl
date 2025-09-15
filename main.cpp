4aa6a20da49f90cfda4828f2798cb9243d9e61ee
Duy Nam <phannam03062006t2@gmail.com>
2025-09-16 03:37:44 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,54 @@
 #include <SFML/Graphics.hpp>
+#include <conio.h>
 #include <iostream>
-using namespace std;
+#include <fstream>
+using namespace sf;
+using namespace syd;
 
-class Player{
-	private:
-    protect:
-    public:
-};
+// map  ? x ? : 1 ô = ? bit
 
-class Enermy : public Player{
+// --------------------------------1-------------------------------------
+      /*
+            -Class : Player (có animation đơn giản)
+            - Sử dụng tính kế thừa, đa hình (CLO2)
+      */
 
-};
-
-class Bomb{
-
-};
+//---------------------------------2-------------------------------------
+      /*
+            - Class : Enermy (2 -> 3 loại quái) (có animation đơn giản )
+            - Sử dụng tính kế thừa, đa hình (CLO2)
+      */
+   
+//---------------------------------3------------------------------------
+      /*
+            - Class : Bomb, Wall
+            - Sử dụng (set, map, queue,...) quản lý đạn, điểm số (CLO3)
+            - Vào ra file (map, cau hình), ngoại lệ (CLO4)
+      */
 
 int main(){
+	// tạo màn hình window
+	RenderWindow window(VideoMode(800, 600), "Muslim");
 	
+	while(window.isOpen()){
+		//-------------------------------Sự kiện---------------------------------------------------------
+		Event event;
+		while(window.pollEvent(event))
+		{
+			if(event.type == Event::Closed)
+			{window.close();}
+			
+		}
+	    //------------------------------Cập nhập --------------------------------------------------------
+
+		
+		
+		//-------------------------------Vẽ------------------------------------------------------------
+		window.clear();
+		window.display();
+		
+	}
+	return 0;
 }
-
-
 
 
