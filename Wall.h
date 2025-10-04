5412c7475edf1939ab992320fdb5739db98d456b
huy-netizen <haquanghuy672006@gmail.com>
2025-10-04 12:45:27 +0700

    Wall.h

--- Wall.h
+++ Wall.h
@@ -11,9 +11,11 @@ class Wall{
 		Texture ket_cau;
 		bool co_the_pha;
 	public:
+		Wall();
 		Wall(Vector2f vitri, bool pha_duoc, const string& duongdan_anh);
 		void ve(RenderWindow& cua_so);
 		FloatRect layKhung();
 		bool laPhaDuoc();
 };
 #endif
+
