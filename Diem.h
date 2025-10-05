73d76b74af8e9baecc8fd516ed5c0eec7cf632d0
huy-netizen <haquanghuy672006@gmail.com>
2025-10-05 16:48:00 +0700

    Update Diem.h

--- Diem.h
+++ Diem.h
@@ -1,20 +1,23 @@
 #ifndef DIEM_H
 #define DIEM_H
-#include<SFML/Graphics.hpp>
-#include<string>
+
+#include <SFML/Graphics.hpp>
+#include <string>
+using namespace sf;
 using namespace std;
-class Diem{
-	private:
-		int points;
-		Font font;
-		Text text;
-		
-	public:
-		Diem();
-		void add(int p);
-		int get();
-		void draw(RenderWindow& window);
-		void save(const string& filename);
+
+class Diem {
+private:
+    int points;
+    Font font;
+    Text text;
+
+public:
+    Diem();
+    void add(int p);
+    int get();
+    void draw(RenderWindow& window);
+    void save(const string& filename);
 };
 
 #endif
