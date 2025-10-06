958a0108fec22becd2526ffb3c522fc48adb88a0
huy-netizen <haquanghuy672006@gmail.com>
2025-10-06 23:52:51 +0700

    Update Map.h

--- Map.h
+++ Map.h
@@ -1,22 +1,22 @@
 #ifndef MAP_H
 #define MAP_H
-#include<SFML/Graphics.hpp>
-#include<map>
+
+#include <SFML/Graphics.hpp>
+#include <map>
 #include "Wall.h"
 using namespace std;
 using namespace sf;
 
-class Map{
-	private:
-		map<pair<int,int>, Wall> ds_tuong;
-		int hang, cot;
-		
-	public:
-		Map();
-		void napFile(const string& tenFile);
-		void ve(RenderWindow& cua_so);
-		map<pair<int,int>, Wall>& layDanhSach();
+class Map {
+private:
+    map<pair<int,int>, Wall> ds_tuong;  // chứa vật thể (có thể vẽ)
+    int hang, cot;
+
+public:
+    Map();
+    void napFile(const string& tenFile);
+    void ve(RenderWindow& cua_so);
+    map<pair<int,int>, Wall>& layDanhSach();
 };
 
 #endif
-
