65fbda345488330f9f0f730e805ecc122278e21c
huy-netizen <haquanghuy672006@gmail.com>
2025-10-07 00:27:14 +0700

    Update Wall.h

--- Wall.h
+++ Wall.h
@@ -1,27 +1,28 @@
 #ifndef WALL_H
 #define WALL_H
-
 #include <SFML/Graphics.hpp>
 #include <string>
-using namespace std;
 using namespace sf;
+using namespace std;
 
 class Wall {
 private:
-    Sprite hinh;
-    Texture anh;
-    bool pha_duoc;   // có thể bị phá khi bomb nổ không
-    bool di_qua;     // có thể đi qua được không
-    Vector2f vitri;
+    RectangleShape hinh;  // Dạng fallback (ô vuông màu)
+    Sprite anh;           // Dùng khi có texture (ảnh)
+    Texture ket_cau;      // Dữ liệu hình ảnh (texture)
+    Vector2f vitri;       // Vị trí trên bản đồ
+    bool co_the_pha;      // Có thể phá được không
+    bool di_qua;          // Có thể đi qua được không
 
 public:
-    Wall();
-    Wall(Vector2f vitri, bool pha_duoc, const string& duongdan_anh, bool di_qua = true);
+    Wall(); 
+    Wall(Vector2f vitri, bool pha_duoc, const string& duongdan_anh, bool di_qua);
 
-    void ve(RenderWindow& window);
-    bool coThePha() const;
-    bool coTheDiQua() const;
-    Vector2f layViTri() const;
+    void ve(RenderWindow& cua_so);
+    FloatRect layKhung();
+
+    bool laPhaDuoc();
+    bool coTheDiQua();
 };
 
 #endif
