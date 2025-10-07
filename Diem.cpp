8d335327837db399ce8e143b38e24f925401aba1
huy-netizen <haquanghuy672006@gmail.com>
2025-10-07 13:16:51 +0700

    Update Diem.cpp

--- Diem.cpp
+++ Diem.cpp
@@ -1,30 +1,36 @@
 #include "Diem.h"
-#include<fstream>
-#include<stdexcept>
+#include <string>
+#include <fstream>
+#include <stdexcept>
 using namespace std;
+
 Diem::Diem() {
-	points=0;
-	if(!font.loadFromFile("arial.ttf")) {
-		throw runtime_error("Font arial.ttf not found!");
-	}
-	text.setFont(font);
-	text.setCharacterSize(20);
-	text.setFillColor(Color::Red);
-	text.setPosition(10,10);
+    points = 0;
+    if (!font.loadFromFile("arial.ttf")) {
+        throw runtime_error("Font arial.ttf not found!");
+    }
+    text.setFont(font);
+    text.setCharacterSize(20);
+    text.setFillColor(Color::Red);
+    text.setPosition(10, 10);
 }
+
 void Diem::add(int p) {
-	points += p;
-}
-int Diem::get() {return points; }
-
-void Diem::draw(RenderWindow& window) {
-	text.setString("Score: "+to_string(points));
-	window.draw(text);
+    points += p;
 }
 
-void Diem::save(const string& filename) {
-	ofstream fout(filename);
-	if(!fout) throw runtime_error("Khong the viet file diem!");
-	fout<<points;
-	fout.close();
+int Diem::get() {
+    return points;
+}
+
+void Diem::draw(RenderWindow &window) {
+    text.setString("Score: " + to_string(points));
+    window.draw(text);
+}
+
+void Diem::save(const string &filename) {
+    ofstream fout(filename.c_str());
+    if (!fout) throw runtime_error("Khong the viet file diem!");
+    fout << points;
+    fout.close();
 }
