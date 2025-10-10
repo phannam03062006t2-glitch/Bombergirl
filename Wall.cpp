0bd29d8181048e639ddf21ab7e299206e5eba630
huy-netizen <haquanghuy672006@gmail.com>
2025-10-10 17:07:32 +0700

    Update Wall.cpp

--- Wall.cpp
+++ Wall.cpp
@@ -1,38 +1,49 @@
 #include "Wall.h"
 #include <iostream>
-using namespace std;
 
-Wall::Wall() {}
+Wall::Wall() : position(0,0), loai(0), ton_tai(true) {}
 
-Wall::Wall(Vector2f vitri, bool pha_duoc, const string& duongdan_anh, bool di_qua)
-    : vitri(vitri), co_the_pha(pha_duoc), di_qua(di_qua)
+Wall::Wall(sf::Vector2f pos, int loai, const std::string& duong_dan_anh)
+    : position(pos), loai(loai), ton_tai(true)
 {
-    hinh.setSize(Vector2f(64, 64)); // set kich thuoc cua hinh
-    hinh.setPosition(vitri); // set vi tri cua hinh
+    if (!texture.loadFromFile(duong_dan_anh)) {
+        std::cout << "Khong load duoc anh: " << duong_dan_anh << std::endl;
+        // Có thể xử lý bằng màu mặc định nếu cần
+    }
+    sprite.setTexture(texture);
+    sprite.setPosition(position);
+    sprite.setScale(64.f / texture.getSize().x, 64.f / texture.getSize().y);
+}
 
-    if (!ket_cau.loadFromFile(duongdan_anh)) { // neu khong tai duoc anh thi tao mau
-        if (pha_duoc) hinh.setFillColor(Color(139, 69, 19)); // mau nau cho tuong pha duoc
-        else hinh.setFillColor(Color(128, 128, 128)); // mau xam cho tuong khong pha duoc
-    } else {
-        anh.setTexture(ket_cau); // set anh cho tuong
-        anh.setPosition(vitri); // set vi tri cho anh
-        anh.setScale(64.f / ket_cau.getSize().x, 64.f / ket_cau.getSize().y); // dieu chinh ti le cua anh
+void Wall::ve(sf::RenderWindow& window) {
+    if (ton_tai) {
+        window.draw(sprite);
     }
 }
 
-void Wall::ve(RenderWindow& cua_so) {
-    if (ket_cau.getSize().x > 0) // neu anh co size hop le thi ve anh
-        cua_so.draw(anh);
-    else // neu khong co anh thi ve hinh chu nhat
-        cua_so.draw(hinh);
+sf::FloatRect Wall::getBounds() const {
+    return sprite.getGlobalBounds();
 }
 
-FloatRect Wall::layKhung() {
-    if (ket_cau.getSize().x > 0) // neu anh co size hop le thi tra ve khung cua anh
-        return anh.getGlobalBounds();
-    else // neu khong co anh thi tra ve khung cua hinh
-        return hinh.getGlobalBounds();
+bool Wall::coTheDiQua() const {
+    // Nền (0) và cỏ (3) thì đi qua được, còn lại không
+    if (!ton_tai) return true;  // nếu đã phá thì đi qua được
+    return (loai == 0 || loai == 3);
 }
 
-bool Wall::laPhaDuoc() { return co_the_pha; } // kiem tra xem tuong co the pha duoc khong
-bool Wall::coTheDiQua() { return di_qua; } // kiem tra xem tuong co the di qua khong
+bool Wall::coThePha() const {
+    // Tường phá được (1) và cây (4) thì có thể phá
+    return (ton_tai && (loai == 1 || loai == 4));
+}
+
+bool Wall::tonTai() const {
+    return ton_tai;
+}
+
+void Wall::pha() {
+    ton_tai = false;
+}
+
+int Wall::getLoai() const {
+    return loai;
+}
