c59bba419f3293999ddebc2834b46bad9c7b1d15
huy-netizen <haquanghuy672006@gmail.com>
2025-10-15 07:00:58 +0700

    Update Map.h

--- Map.h
+++ Map.h
@@ -1,26 +1,32 @@
 #ifndef MAP_H
 #define MAP_H
+
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <string>
 #include "Wall.h"
+
 using namespace std;
 using namespace sf;
 
 class Map {
 private:
     vector<Wall> danh_sach_tuong_pha_duoc;
-    vector<Wall> danh_sach_tuong_khong_pha;
-
-    vector<Wall> danh_sach_phan_khac; // gồm ground, grass, tree...
+    vector<Wall2> danh_sach_tuong_khong_pha;
 
 public:
     Map();
     Map(const string& duongdan_file);
+
     void napFile(const string& duongdan_file);
-    void ve(RenderWindow& cua_so);
+
+ 
+    void ve(RenderWindow& window);
+
 
     vector<Wall>& layDanhSachTuongPhaDuoc();
-    vector<Wall>& layDanhSachTuongKhongPha();
+    vector<Wall2>& layDanhSachTuongKhongPha();
+
 
     bool kiemTraVaCham(const FloatRect& khung_nv);
 };
