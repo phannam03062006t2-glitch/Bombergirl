71bbfa9d7315e8e4ffbfd99aec8193ac3e6f0564
huy-netizen <haquanghuy672006@gmail.com>
2025-10-15 07:40:06 +0700

    Update Wall.h

--- Wall.h
+++ Wall.h
@@ -6,18 +6,19 @@ using namespace std;
 using namespace sf;
 class Wall {
 public:
-    float x, y;
-    static Texture TEXTURE;
+    float x, y;        // Toa do tuong
+    static Texture TEXTURE; // Anh dung cho tat ca tuong
     Sprite SPRITE;
 
-    Wall(float x, float y);
+    Wall(float x, float y);  // Tao tuong tai toa do
     void Ve(sf::RenderWindow &window);
 };
 
 class Wall2 : public Wall {
 public:
-    static sf::Texture TEXTURE;
+    static Texture TEXTURE; Anh rieng dung cho Wall2
     Wall2(float x, float y);
 };
 
 #endif
+
