3b8ee4cb0e3b6d4dd65e331971a384924e41efe6
Duy Nam <phannam03062006t2@gmail.com>
2025-10-16 17:33:26 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include "Player.h"
-#include "Enemy.h"
-#include "Map.h"
+#include "Map_phu.h"
 #include "Diem.h"
 // ===================================
 
 using namespace std;
 using namespace sf;
 
-Map mapGame;
 Diem diem;
 
-extern vector<Bomb> QuanLyBomb;
-extern vector<Enemy> DanhSachEnemy;
+
+//extern vector<Enemy1> DanhSachEnemy1;
+//extern vector<Enemy2> DanhSachEnemy2;
+//extern vector<Enemy3> DanhSachEnemy3;
 
 int main() {
     RenderWindow window(VideoMode(1700, 900), "my game");
@@ -23,12 +23,7 @@ int main() {
     srand(time(0));
 
     // Nap map
-    try {
-        mapGame = Map("map.txt");
-    } catch (runtime_error& e) {
-        cerr << "Loi map: " << e.what() << endl;
-        return -1;
-    }
+    input_map();
 
     Player a;
     float deltaTime = 0.f;
@@ -36,16 +31,12 @@ int main() {
     float Time = 0.f;
 
     Texture TEXTURE;
-    TEXTURE.loadFromFile("map.png");
+    TEXTURE.loadFromFile("assets/map.png");
     Sprite SPRITE(TEXTURE);
 
     // ==========================
     // ?? Thêm 3 quái khác hình
     // ==========================
-    DanhSachEnemy.clear();
-    DanhSachEnemy.push_back(Enemy(200, 200, 0)); // enemy1.png
-    DanhSachEnemy.push_back(Enemy(600, 400, 1)); // enemy2.png
-    DanhSachEnemy.push_back(Enemy(1000, 600, 2)); // enemy3.png
     // ==========================
 
     while (window.isOpen()) {
@@ -60,42 +51,12 @@ int main() {
         // Cap nhat bomb & player
         CapNhapBomb(QuanLyBomb, deltaTime);
         CapNhapPlayer(a);
-
-        // Cap nhat enemy
-        for (auto &enemy : DanhSachEnemy) {
-            enemy.capNhat(window);
-            if (enemy.kiemTraVaChamPlayer(a.SPRITE.getGlobalBounds())) {
-                a.alive = false;
-            }
-        }
-
-        // Kiem tra enemy chet khi trúng bomb
-        for (auto &enemy : DanhSachEnemy) {
-            for (auto &bomb : QuanLyBomb) {
-                if (bomb.dangNo) {
-                    FloatRect bomNo(
-                        bomb.x - 64 * bomb.phamVi,
-                        bomb.y - 64 * bomb.phamVi,
-                        64 * (2 * bomb.phamVi + 1),
-                        64 * (2 * bomb.phamVi + 1)
-                    );
-                    enemy.kiemTraVaChamBom(bomNo);
-                }
-            }
-        }
-
-        // ==========================
-        //  Ve moi thu
+        
         // ==========================
         window.clear();
 
-        
-
         // Ve background map.png
         window.draw(SPRITE);
-
-        // Ve cac doi tuong
-        mapGame.ve(window);
 
         // Ve player, bom, quai,..
         for (auto &bomb : QuanLyBomb)
@@ -103,14 +64,13 @@ int main() {
 
         a.Ve(window, Time);
 
-        for (auto &enemy : DanhSachEnemy)
-            enemy.Ve(window);
-
-        // Ve diem len goc man 
+        // Ve cac doi tuong
+        veWall(window);
+        
+        // Ve diem
         diem.draw(window);
 
         window.display();
     }
     return 0;
 }
-
