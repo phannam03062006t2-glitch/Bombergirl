7312e0d1433e0142823c8f6b0ef888b7752f83ee
huy-netizen <haquanghuy672006@gmail.com>
2025-10-20 02:05:53 +0700

    Update Map_phu.h

--- Map_phu.h
+++ Map_phu.h
@@ -3,34 +3,32 @@
 
 #include <vector>
 #include <SFML/Graphics.hpp>
-#include <stdexcept> // thêm d? dùng exception
 using namespace std;
 using namespace sf;
 
-class Wall {
-public:
-    float x, y;
-    float c1, c2, c3, c4;
-    static Texture TEXTURE;
-    Sprite SPRITE;
 
-    Wall(float x, float y);
-    void Ve(RenderWindow &window);
+class Wall{
+   public:
+     float x, y;
+     float c1, c2, c3, c4;
+	 static Texture TEXTURE;
+	 Sprite SPRITE;
+	Wall(float x, float y);	
+	void Ve(RenderWindow &window);
 };
 
-class Wall2 : public Wall {
-public:
-    static Texture TEXTURE;
-    Wall2(float x, float y);
+class Wall2 : public Wall{
+	public:
+		static Texture TEXTURE;
+		Wall2(float x, float y) : Wall(x, y){
+			TEXTURE.loadFromFile("assets/wall2.png");
+	         SPRITE.setTexture(TEXTURE);
+		}
 };
 
-// Bi?n toàn c?c
 extern vector<Wall> QuanLyWall;
 extern vector<Wall2> QuanLyWall2;
-
-// Hàm x? lý b?n d?
 void input_map();
 void veWall(RenderWindow &window);
 
 #endif
-
