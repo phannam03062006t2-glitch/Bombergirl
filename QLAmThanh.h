08cb21e7b4512547f7f0823bb6f7f6b5f953a84d
huy-netizen <haquanghuy672006@gmail.com>
2025-10-20 01:49:04 +0700

    Update QLAmThanh.h

--- QLAmThanh.h
+++ QLAmThanh.h
@@ -11,22 +11,15 @@ class QLAmThanh {
 private:
     map<string, SoundBuffer> buffers; // Luu cac tep am da nap
     vector<Sound> sounds;             // Danh sach cac am dang phat
-
+	Music nhacNen;
 public:
-    // Nap am thanh tu tep vao bo nho
-    void napAm(const string& ten, const string& duongdan);
-
-    // Phat mot am thanh ngan theo ten da nap
-    void phatAm(const string& ten);
-
-    // Phat nhac nen (lap vo han)
-    void phatNhacNen(const string& duongdan);
-    
-    //Dung tat ca am thanh
-    void dungTatCaAmThanh(); // Khai báo
-
-    // Cap nhat, xoa cac am thanh da phat xong
-    void capNhat();
+    void napAm(const string& ten, const string& duongdan); // Nap am thanh tu tep vao bo nho
+    void phatAm(const string& ten); // Phat mot am thanh ngan theo ten da nap
+	void phatNhacNen(const string& duongdan);  // Phat nhac nen (lap vo han)
+    void dungTatCaAmThanh(); //Dung tat ca am thanh
+    void tamDungTatCaAmThanh();
+	void tiepTucTatCaAmThanh();
+	void capNhat(); // Cap nhat, xoa cac am thanh da phat xong
 };
 
 // Bien am thanh toan cuc de goi tu nhieu file khac nhau
